e4403209771f8a49a5082b3c7fab8a2e3bff6974
Stanislav Kasprik <stanislav.kasprik@gmail.com>
2019-10-05 20:01:04 +0200

    Initial Commit

---
+++ Prototype2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototype2
+//
+//  Created by Stanislav Kasprik on 05/10/2019.
+//  Copyright © 2019 stanislavK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
